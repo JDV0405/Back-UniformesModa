77605c91c732ef7743db16db88387907efdc720e
Victor-Manuel-David <victordavid323045@correo.itm.edu.co>
2025-05-05 10:57:57 -0500

    refactor: remove getUserbyId model file

--- models/getUserbyId.model.js
+++
@@ -1,6 +0,0 @@
-const pool = require('../database/db.js'); // Asegúrate de que la ruta sea correcta
-
-const buscarClientePorCedula = async (cedula) => {
-  const result = await pool.query('SELECT * FROM cliente WHERE cedula = $1', [cedula]);
-  return result.rows[0];
-};
